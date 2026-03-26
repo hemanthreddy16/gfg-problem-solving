002ed60c11b0375be3b3c804ddcc3a094270af5d
hemanthreddy16 <reddamhemanth@gmail.com>
2026-03-26 14:39:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Pattern 13/pattern-13.java
@@ -0,0 +1,15 @@
+//Back-end complete function Template for Java
+
+class Solution {
+
+    void printTriangle(int n) {
+        int num=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(num+" ");
+                num=num+1;
+                
+            }System.out.println();
+        }
+    }
+}
