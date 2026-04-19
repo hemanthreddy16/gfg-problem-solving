a447c4ad2202edd71663bbd6bc3c2ccc7756c36f
Hemanth Reddy <reddamhemanth@gmail.com>
2026-04-19 21:16:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Rotate Array by One/rotate-array-by-one.java
+++ Difficulty: Basic/Rotate Array by One/rotate-array-by-one.java
@@ -5,7 +5,9 @@ class Solution {
         // code here
         int temp=arr[arr.length-1];
         for(int i=arr.length-1;i>=1;i--){
-            arr[i]=arr[i-1];}
+            arr[i]=arr[i-1];
+            
+        }
             arr[0]=temp;
     
         
