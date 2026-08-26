fecf13cae91c8fbecc2a518e4c8c50f2096a8b7a
Hemanth Reddy <reddamhemanth@gmail.com>
2026-08-26 14:57:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Number of Occurrence/number-of-occurrence.java
@@ -0,0 +1,12 @@
+class Solution {
+    int countFreq(int[] arr, int target) {
+        // code here
+        int n=arr.length;
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==target){
+                c++;
+            }
+        }return c;
+    }
+}
