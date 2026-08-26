7805819fb12e2597c5832ddddf59fd66a0d12968
Hemanth Reddy <reddamhemanth@gmail.com>
2026-08-26 11:27:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement Lower Bound/implement-lower-bound.java
@@ -0,0 +1,18 @@
+class Solution {
+    int lowerBound(int[] arr, int target) {
+        // code here
+        int n=arr.length;
+        int low=0;
+        int high=n-1;
+        int ans=n;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]>=target){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }return ans;
+    }
+}
